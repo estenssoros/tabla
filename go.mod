9d4cc793c373c16ccd46ad349ee8b59c268eabb5
estenssoros <seb.estenssoro@gmail.com>
2023-05-22 07:33:44 -0600

    logrus

--- go.mod
+++ go.mod
@@ -7,7 +7,8 @@ require (
 	github.com/denisenkom/go-mssqldb v0.0.0-20191128021309-1d7a30a10f73
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.2
 	github.com/spf13/cobra v0.0.5
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.7.0
 	github.com/xwb1989/sqlparser v0.0.0-20180606152119-120387863bf2
 )
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
-	"log"
-
 	"github.com/estenssoros/tabla/cmd"
+	"github.com/sirupsen/logrus"
 )
+
+func init() {
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	logrus.SetFormatter(customFormatter)
+	customFormatter.FullTimestamp = true
+}
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
